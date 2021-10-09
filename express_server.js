7baabfa6818ae70b06d16fbbf17f9e774d3d53c0
Nancy Boktor <nancyboktor1@gmail.com>
2021-10-08 21:14:37 -0400

    Updating my Tinyapp

--- express_server.js
+++ express_server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// require helper functions
+const {
+  generateRandomString,
+  findUserByEmail,
+  truePassword,
+  getURLSByUser,
+  isOwnUrl,
+  isExist,
+} = require("./helpers");
+
 // using ejs templare.
 app.set("view engine", "ejs");
 
@@ -59,35 +69,26 @@ const users = {
   },
 };
 
-// require helper functions
-const {
-  generateRandomString,
-  findUserByEmail,
-  truePassword,
-  getURLSByUser,
-  isOwnUrl,
-  isExist,
-} = require("./helpers");
-
 app.get("/", (req, res) => {
-  const userid = req.session.userid;
-  if (userid) {
+  const userId = req.session.userId;
+  if (userId) {
     return res.redirect("/urls");
   }
   return res.redirect("/login");
 });
 
+// render user's urls if he has created some and he login
 app.get("/urls", (req, res) => {
-  const userid = req.session.userid;
+  const userId = req.session.userId;
 
-  if (!userid) {
+  if (!userId) {
     return res.status(403).send("You should login / register first.");
   }
 
-  const urls = getURLSByUser(userid, urlDatabase);
+  const urls = getURLSByUser(userId, urlDatabase);
 
   const templateVars = {
-    user: users[userid],
+    user: users[userId],
     urls,
     notExist: Object.keys(urls).length ? "" : "Please create your first url",
   };
@@ -95,18 +96,20 @@ app.get("/urls", (req, res) => {
   return res.render("urls_index", templateVars);
 });
 
+//render the page for creating new shortUrl
 app.get("/urls/new", (req, res) => {
-  const userid = req.session.userid;
-  const templateVars = { user: users[userid] };
-  if (!userid) {
+  const userId = req.session.userId;
+  const templateVars = { user: users[userId] };
+  if (!userId) {
     return res.redirect("/login");
   }
 
   res.render("urls_new", templateVars);
 });
 
+// user can creat his own shortUrls within this post req
 app.post("/urls", (req, res) => {
-  const userID = req.session.userid;
+  const userID = req.session.userId;
   const longURL = req.body.longURL;
   const shortURLId = generateRandomString();
   if (!userID) {
@@ -119,6 +122,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURLId}`);
 });
 
+// pressing on your shortURL within /u/:shortURL sending you to the main page for this shortURL
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
 
@@ -130,39 +134,32 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//render the user's shortUrl
 app.get("/urls/:shortURL", (req, res) => {
-  const userid = req.session.userid;
-
-  if (!userid) {
-    return res.status(403).send("You should login / register first.");
+  const userId = req.session.userId;
+  const shortURL = req.params.shortURL;
+  if (userId === urlDatabase[shortURL].userID) {
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL,
+      user: users[userId],
+    };
+    res.render("urls_show", templateVars);
+    return;
   }
-
-  if (!isExist(req.params.shortURL, urlDatabase)) {
-    return res.status(404).send("This url is not exist");
-  }
-
-  if (!isOwnUrl(userid, req.params.shortURL, urlDatabase)) {
-    return res.status(403).send("You can't access this url");
-  }
-
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
-    user: users[userid],
-  };
-
-  res.render("urls_show", templateVars);
+  return res.status(404).send("You don't own this shortURL!!!");
 });
 
+// user can delete his own shortURLs within this post req
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userid = req.session.userid;
+  const userId = req.session.userId;
   const url = req.params.shortURL;
-  if (!userid) {
+  if (!userId) {
     return res
       .status(400)
       .send("You need to register or login first! Thank you");
   }
-  if (!isOwnUrl(userid, url, urlDatabase)) {
+  if (!isOwnUrl(userId, url, urlDatabase)) {
     return res.status(403).send("You can't access this url");
   }
 
@@ -170,16 +167,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//when the user want to get his shortURL
 app.post("/urls/:id", (req, res) => {
-  const userid = req.session.userid;
+  const userId = req.session.userId;
   const shortUrlId = req.params.id;
 
-  if (!userid) {
+  if (!userId) {
     return res
       .status(400)
       .send("You need to register or login first! Thank you");
   }
-  if (!isOwnUrl(userid, shortUrlId, urlDatabase)) {
+  if (!isOwnUrl(userId, shortUrlId, urlDatabase)) {
     return res.status(403).send("You can't access this url");
   }
   const newURL = req.body.newUrl;
@@ -189,15 +187,18 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+//render the register page
 app.get("/register", (req, res) => {
-  const userid = req.session.userid;
-  if (userid) {
+  const userId = req.session.userId;
+  if (userId) {
     return res.redirect("/urls");
   }
   const templateVars = { user: null };
   res.render("urls_register", templateVars);
 });
 
+// new user creating his account within this post req
+// and creating his cookie
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
 
@@ -209,47 +210,52 @@ app.post("/register", (req, res) => {
   } else if (userFound) {
     return res.status(400).send("Sorry, that user already exists!");
   } else {
-    let userid = generateRandomString();
-    users[userid] = {
-      id: userid,
+    const userId = generateRandomString();
+    users[userId] = {
+      id: userId,
       email,
       password: hashPassword,
     };
 
-    req.session.userid = userid;
+    req.session.userId = userId;
 
     res.redirect("/urls");
   }
 });
 
+//render the login page
 app.get("/login", (req, res) => {
-  const userid = req.session.userid;
-  if (userid) {
+  const userId = req.session.userId;
+  if (userId) {
     return res.redirect("/urls");
   }
   const templateVars = { user: null };
   res.render("urls_login", templateVars);
 });
 
+//user which has an account able to login within this post req
 app.post("/login", (req, res) => {
-  let { email, password } = req.body;
+  const { email, password } = req.body;
 
   const userFound = findUserByEmail(email, users);
   const passwordFound = truePassword(password, userFound.password);
+
   if (userFound && passwordFound) {
-    req.session.userid = userFound["id"];
+    req.session.userId = userFound["id"];
 
     return res.redirect("/urls");
   }
 
-  res.send("uncorrect password");
+  res.status(403).send("Email cannot be found or wrong password/email");
 });
 
+//user can logout within this post req
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+//server is lisening
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
